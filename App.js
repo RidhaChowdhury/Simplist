0d2211ce55aa3f9bcb19094bb333db3e1699dbd4
Ridha Chowdhury <rac6115@psu.edu>
2024-06-10 02:17:43 -0500

    Initial commit. Setting up project.
    Update npm dependencies to latest stable versions, including @eva-design/eva, @expo/metro-runtime, @ui-kitten/components, react-dom, react-native-animatable, react-native-modal, react-native-svg, react-native-vector-icons, and react-native-web.

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, CheckBox } from '@ui-kitten/components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Welcome to Simplist</Text>
+      </Layout>
+    </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
